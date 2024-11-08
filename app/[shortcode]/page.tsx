3a68b7d609d82311a24ba0aa0aff98aef29a76ab
Toshiaki Iizuka <t_112ka@yahoo.co.jp>
2024-11-08 12:35:27 +0900

    add redirect func

---
+++ app/[shortcode]/page.tsx
@@ -0,0 +1,27 @@
+import prisma from "@/lib/db";
+import { redirect } from "next/navigation";
+
+interface RedirectPageProps {
+  params: { shortcode: string };
+}
+
+const RedirectPage = async ({ params }: RedirectPageProps) => {
+  const { shortcode } = params;
+
+  const url = await prisma.url.findUnique({
+    where: { shortCode: shortcode },
+  });
+
+  if (!url) {
+    return <div>404 - URL not found</div>;
+  }
+
+  await prisma.url.update({
+    where: { id: url.id },
+    data: { visits: { increment: 1 } },
+  });
+
+  redirect(url.originalUrl);
+};
+
+export default RedirectPage;
